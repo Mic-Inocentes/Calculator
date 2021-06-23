8694cedd99bf2e515001c5e7f2b0dcc883bb709f
thisismic <micmic.inocentes@gmail.com>
2021-06-24 02:30:31 +0800

    Continuation of Click Button Functions

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,6 +12,12 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        string first = "";
+        string second = "";
+        char function;
+        double result = 0.0;
+        string UserInput = "";
+
         public Form1()
         {
             InitializeComponent();
@@ -19,87 +25,87 @@ namespace Calculator
 
         private void SymbolKeypad0_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "0";
         }
 
         private void SymbolKeypad1_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "1";
         }
 
         private void SymbolKeypad2_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "2";
         }
 
         private void SymbolKeypad3_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "3";
         }
 
         private void SymbolKeypad4_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "4";
         }
 
         private void SymbolKeypad5_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "5";
         }
 
         private void SymbolKeypad6_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "6";
         }
 
         private void SymbolKeypad7_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "7";
         }
 
         private void SymbolKeypad8_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "8";
         }
 
         private void SymbolKeypad9_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += "9";
         }
 
         private void SymbolButton_Decimal_Click(object sender, EventArgs e)
         {
-
+            CalcDisplay.Text += ".";
         }
 
         private void SymbolButton_Plus_Click(object sender, EventArgs e)
         {
-
+            function = '+';
         }
 
         private void SymbolButton_Minus_Click(object sender, EventArgs e)
         {
-
+            function = '-';
         }
 
         private void SymbolButton_Times_Click(object sender, EventArgs e)
         {
-
+            function = '*';
         }
 
         private void SymbolButton_Divide_Click(object sender, EventArgs e)
         {
-
+            function = '/';
         }
 
         private void Button_AllClear_Click(object sender, EventArgs e)
         {
-
+            function = 'C';
         }
 
         private void SymbolButton_Equal_Click(object sender, EventArgs e)
         {
-
+            function = '=';
         }
     }
 }
